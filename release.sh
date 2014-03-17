938c4aede4407dffea3fe1e9552be96ba21a46fa
Andrey Pushkarev <fealaer@gmail.com>
2014-03-17 22:54:34 +0530

    Ignore .gitignore when commit to release

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 git checkout release
 git merge master -q -m "Merge to release"
 grunt build
-git add dist -A
+git add dist -Af
 git commit -m 'New Build'
 git push heroku release:master
 git checkout -
